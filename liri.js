1994edb0ba7b8e9e4101c09982676060a7267591
Randolph Bailey <randolphbailey@gmail.com>
2019-02-19 17:20:42 -0600

    initial code buildout

--- liri.js
+++ liri.js
@@ -1,3 +1,44 @@
-require("dotevn").config();
+require("dotenv").config();
 
-var keys = require("./keys.js");
+var keys = require("./keys");
+
+var axios = require("axios");
+var spotify = require("node-spotify-api");
+var moment = require("moment");
+var spotify = new Spotify(keys.spotify);
+
+var action = process.argv[2];
+var query = process.argv[3];
+
+switch (action) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        doWhat();
+        break;
+    default:
+        console.log("Invalid command");
+}
+
+function concertThis() {
+
+}
+
+function spotifyThis() {
+
+}
+
+function movieThis() {
+
+}
+
+function doWhat() {
+
+}
